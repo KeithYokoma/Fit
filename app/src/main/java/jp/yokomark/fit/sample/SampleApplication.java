f97c9222e16489bfb6e9528efa9d4ec44a001f82
KeithYokoma <Keith.Yokoma@gmail.com>
2016-01-22 14:51:54 +0900

    Update sample app

--- app/src/main/java/jp/yokomark/fit/sample/SampleApplication.java
+++ app/src/main/java/jp/yokomark/fit/sample/SampleApplication.java
@@ -1,8 +1,10 @@
 package jp.yokomark.fit.sample;
 
 import android.app.Application;
+import android.util.Log;
 
 import jp.yokomark.fit.Fit;
+import jp.yokomark.fit.Result;
 
 /**
  * @author KeithYokoma
@@ -14,7 +16,8 @@ public class SampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Fit.initialize(this, new SampleModule());
-        Fit.getInstance().execute();
+        Result result = Fit.getInstance().execute();
+        Log.v(TAG, "upgraded from " + result.getFrom());
     }
 
     @Override
