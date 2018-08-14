fb84a6c0209c08edced15098376d09199f75da36
KeithYokoma <Keith.Yokoma@gmail.com>
2018-08-14 10:02:52 +0900

    Update build environment

--- app/src/main/java/jp/yokomark/fit/sample/SampleActivity.java
+++ app/src/main/java/jp/yokomark/fit/sample/SampleActivity.java
@@ -1,12 +1,12 @@
 package jp.yokomark.fit.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * @author KeithYokoma
  */
-public class SampleActivity extends ActionBarActivity {
+public class SampleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
